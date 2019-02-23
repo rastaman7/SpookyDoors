388ca45f504f5b5503003debb099a292cb39baf1
rastaman7 <34154020+rastaman7@users.noreply.github.com>
2019-02-23 11:35:36 +0900

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -1,31 +1,21 @@
 import UIKit
-import AVFoundation
 
 class ViewController: UIViewController {
     
-    var audioPlayer = AVAudioPlayer()
+    var soundObj = sound()
     
     @IBOutlet weak var Start6: UIButton!
     @IBAction func Start6Pressed() {
-        audioPlayer.play()
+        soundObj.buttonSound()
     }
     @IBOutlet weak var Settings: UIButton!
     @IBAction func SettingsPressed() {
-        audioPlayer.play()
+        soundObj.buttonSound()
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let sound = Bundle.main.path(forResource: "buttonSound", ofType: "mp3")
-        
-        do{
-            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
-            audioPlayer.prepareToPlay()
-        }catch{
-            print(error)
-        }
         
         // Do any additional setup after loading the view, typically from a nib.
     }
